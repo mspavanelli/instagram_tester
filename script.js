848e0d277d6e82415f60cd0c0b4397e3378d6321
Matheus Pavanelli <matheus.pavanelli@madeinweb.com.br>
2022-03-31 13:59:33 -0300

    feat: add error message

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ function startVideoFromCamera() {
       const videoElement = document.querySelector("#video");
       videoElement.srcObject = stream;
     })
-    .catch((err) => console.error(err));
+    .catch((err) => {
+      const $errors = document.querySelector("#errors");
+      $errors.innerHTML = err;
+    });
 }
 
 window.addEventListener("DOMContentLoaded", startVideoFromCamera);
@@ -13,7 +16,6 @@ window.addEventListener("DOMContentLoaded", startVideoFromCamera);
 function displayInfo() {
   const { userAgent } = navigator;
   const $info = document.querySelector("#info");
-
   $info.innerHTML = userAgent;
 }
 
